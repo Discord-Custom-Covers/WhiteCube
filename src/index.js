32fd569b9b6f1c37f83457ab31ece3f6743beee3
Strencher <46447572+Strencher@users.noreply.github.com>
2021-06-20 13:43:09 +0200

    Fix request author
    Sorry, fixed now.,

--- src/index.js
+++ src/index.js
@@ -225,7 +225,7 @@ var queueBusy = false;
     var values = [
       {
         name: "Author",
-        value: `<@!${msg.author.tag}> (${msg.author.id})`,
+        value: `${msg.author.mention} (${msg.author.username}#${msg.author.discriminator})`,
       },
       {
         name: "Background",
